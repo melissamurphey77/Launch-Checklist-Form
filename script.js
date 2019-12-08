5eea94d3db0fe45c2a2f1ca6edb3182f7cf01656
Melissa Murphey <melissamurphey77@gmail.com>
2019-12-08 15:43:29 -0600

    adding validation of input

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ window.addEventListener("load", function() {
    form.addEventListener("submit", function(event) {
       let pilotName = document.querySelector("input[name=pilotName]");
       let coPilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
-      if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+      let fuelLevel =  document.querySelector("input[name=fuelLevel]");
+      let cargoMass =  document.querySelector("input[name=cargoMass]");
+      if (pilotName.value === "" || coPilotName.value === "") {
          alert("All fields are required!");
          // stop the form submission
          event.preventDefault();
       }
-   });
- });
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+      if(fuelLevel.value === "" || cargoMass.value === ""){
+         alert("All fields are required!");
+         // stop the form submission
+         event.preventDefault();
+      }else if((fuelLevel = isNaN(value)) || (cargoMass = isNaN(value))){
+         alert("Fuel Level and/or Cargo Mass input is not a Number");
+         // stop the form submission
+         event.preventDefault();
+      }
+      });
+
+      
+      
+   });
+
+   
+
+
+
+// This block of code shows how to format the HTML once you fetch some planetary JSON!
+// <h2>Mission Destination</h2>
+// <ol>
+//    <li>Name: ${}</li>
+//    <li>Diameter: ${}</li>
+//    <li>Star: ${}</li>
+//    <li>Distance from Earth: ${}</li>
+//    <li>Number of Moons: ${}</li>
+// </ol>
+// <img src="${}">
+
